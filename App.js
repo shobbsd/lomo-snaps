2a715e9eed1a2d5eb833ffa536d86fb10ee3ed42
CKeogh <c.keogh.10.ck@gmail.com>
2019-04-03 15:33:46 +0100

    setup basic navigation template

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import Camera from './Components/Camera';
+import SignUp from './Components/SignUp';
+import NewEvent from './Components/NewEvent';
+import Gallery from './Components/Gallery';
 
-export default class App extends React.Component {
+class Home extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>This is the home screen</Text>
+        <Button
+          title='open camera'
+          onPress={() => {
+            this.props.navigation.navigate('Camera')
+          }}
+        />
+        <Button
+          title='open SignUp'
+          onPress={() => {
+            this.props.navigation.navigate('SignUp')
+          }}
+        />
+        <Button
+          title='open new event'
+          onPress={() => {
+            this.props.navigation.navigate('NewEvent')
+          }}
+        />
+        <Button
+          title='open Gallery'
+          onPress={() => {
+            this.props.navigation.navigate('Gallery')
+          }}
+        />
+
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Home
   },
+  Camera: {
+    screen: Camera
+  },
+  SignUp: {
+    screen: SignUp
+  },
+  NewEvent: {
+    screen: NewEvent
+  },
+  Gallery: {
+    screen: Gallery
+  }
 });
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
---
+++ Components/NewEvent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class NewEvent extends Component {
+    render() {
+        return (
+            <View>
+                <Text>This is the New Event page</Text>
+            </View>
+        )
+    }
+}
