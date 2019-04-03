79a1bd21a2a535dff96c2099e8f5fb2a67ec1dd3
Shaq Hobbs <41305147+shobbsd@users.noreply.github.com>
2019-04-03 17:08:21 +0100

    firebase Config set up (#4)

--- App.js
+++ App.js
@@ -1,43 +1,58 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import Camera from './Components/Camera';
-import SignUp from './Components/SignUp';
-import NewEvent from './Components/NewEvent';
-import Gallery from './Components/Gallery';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Camera from "./Components/Camera";
+import SignUp from "./Components/SignUp";
+import NewEvent from "./Components/NewEvent";
+import Gallery from "./Components/Gallery";
+import firebaseConnect from "./firebaseConfig";
 
 class Home extends Component {
   render() {
+    firebaseConnect
+      .auth()
+      .signInAnonymously()
+      .then(user => console.log(user, "first"));
+    firebaseConnect
+      .auth()
+      .signInWithEmailAndPassword("shobbsdaley@gmail.com", "northCoders")
+      .then(user => console.log(user));
+    // .app("retro_snaps")
+    // .auth()
+    // .("apple@icloud.com", "apple123")
+    // .then(user => {
+    //   console.log(user);
+    //   // console.log("kittensApp user ->", user.toJSON());
+    // });
     return (
       <View>
         <Text>This is the home screen</Text>
         <Button
-          title='open camera'
+          title="open camera"
           onPress={() => {
-            this.props.navigation.navigate('Camera')
+            this.props.navigation.navigate("Camera");
           }}
         />
         <Button
-          title='open SignUp'
+          title="open SignUp"
           onPress={() => {
-            this.props.navigation.navigate('SignUp')
+            this.props.navigation.navigate("SignUp");
           }}
         />
         <Button
-          title='open new event'
+          title="open new event"
           onPress={() => {
-            this.props.navigation.navigate('NewEvent')
+            this.props.navigation.navigate("NewEvent");
           }}
         />
         <Button
-          title='open Gallery'
+          title="open Gallery"
           onPress={() => {
-            this.props.navigation.navigate('Gallery')
+            this.props.navigation.navigate("Gallery");
           }}
         />
-
       </View>
-    )
+    );
   }
 }
 
