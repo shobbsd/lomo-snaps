f07b5cd39f731b849972e1e14b56d25302ed28d4
philval <526473+gb96@users.noreply.github.com>
2019-04-04 12:39:08 +0100

    newEvent.js form submits data to firestore(DB). TODO Dates are stored as strings... goto firestore timestamps

--- Components/NewEvent.js
+++ Components/NewEvent.js
@@ -1,44 +1,73 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
+import firebaseConnect from '../firebaseConfig';
+import '@firebase/firestore';
+
+const db = firebaseConnect.firestore();
 
 export default class NewEvent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { eventName: '', eventEndDate: '', eventDevelopDate: '' };
+    constructor() {
+        super();
+        this.state = {
+            eventName: '',
+            eventEndDate: '',
+            eventDevelopDate: '',
+        }
     }
 
-    handleEventSubmit = (event) => {
+    handleEventSubmit = () => {
         console.log(this.state) // logs to expo start console
+        this.addEvent()
+    }
+
+    addEvent = () => {
+        // https://firebase.google.com/docs/firestore/quickstart
+        // NB generated ID
+        // TODO app generates ID ?
+        db.collection('events').add(this.state)
+            .then(function (docRef) {
+                console.log("Document written with ID: ", docRef.id);
+            })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
+            });
     }
 
     render() {
         return (
             <>
                 <Text>
-                    Enter your event name :
+                    Event Name:
                 </Text>
                 <TextInput
                     style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 40, }}
+                    placeholder="Enter your event name.."
                     onChangeText={(eventName) => this.setState({ eventName })}
                     value={this.state.eventName}
                 />
                 <Text>
-                    Event Date as DD/MM/YYYY :
+                    Event End Date:
                 </Text>
                 <TextInput
                     style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 40, }}
+                    placeholder="DD/MM/YYYY"
                     onChangeText={(eventEndDate) => this.setState({ eventEndDate })}
                     value={this.state.eventEndDate}
                 />
                 <Text>
-                    Develop Photos Date as DD/MM/YYYY :
+                    Develop Photos Date:
                 </Text>
                 <TextInput
                     style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 40, }}
+                    placeholder="DD/MM/YYYY"
                     onChangeText={(eventDevelopDate) => this.setState({ eventDevelopDate })}
                     value={this.state.eventDevelopDate}
                 />
-                <Button title="Submit your event" onPress={this.handleEventSubmit} />
+                <Button
+                    style={{ color: 'red', padding: 20, borderWidth: 1, marginBottom: 40, }}
+                    title="Submit your event"
+                    onPress={this.handleEventSubmit}
+                />
             </>
         );
     }
