7e9722cc32f3986507f0484904aae3034d3fc62b
MohammedJubel <jubelm@hotmail.co.uk>
2019-04-05 12:09:19 +0100

    created camera feature

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+// import React from "react";
+// import { View, Image, ScrollView } from "react-native";
+
+// import styles from "../styles/cameraStyle";
+
+// export default ({ captures = [] }) => (
+//   <ScrollView
+//     horizontal={true}
+//     style={[styles.bottomToolbar, styles.galleryContainer]}
+//   >
+//     {captures.map(({ uri }) => (
+//       <View style={styles.galleryImageContainer} key={uri}>
+//         <Image source={{ uri }} style={styles.galleryImage} />
+//       </View>
+//     ))}
+//   </ScrollView>
+// );
+
+import React, { Component } from "react";
+import { View, Text } from "react-native";
 
 export default class Gallery extends Component {
-    render() {
-        return (
-            <View>
-                <Text>This is the Gallery</Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <Text>This is the Gallery</Text>
+      </View>
+    );
+  }
 }
