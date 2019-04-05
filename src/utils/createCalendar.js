908c4b5cbdc0be9039e6d7e44235925377f12631
CKeogh <c.keogh.10.ck@gmail.com>
2019-04-05 16:39:15 +0100

    make createCalendar function check if calendar already exists

--- src/utils/createCalendar.js
+++ src/utils/createCalendar.js
@@ -1,33 +1,41 @@
 import { Calendar } from 'expo';
 
-
-// creates new calendar with title Lomo and returns array with timeZone and id number
+// checks if Lomo calendar exists and if not creates new calendar and returns timeZone and calendar id
 const createCalendar = (username) => {
+
     return Calendar.getCalendarsAsync()
         .then((calendars) => {
-
-            const { source, timeZone, ownerAccount } = calendars[0]
-
-            const calendarId = Calendar.createCalendarAsync({
-                accessLevel: "owner",
-                allowedReminders: [
-                    "default",
-                    "alert",
-                ],
-                allowsModifications: true,
-                color: "#1E7BA8",
-                isPrimary: false,
-                isSynced: true,
-                isVisible: true,
-                name: `Lomo-${username}`,
-                ownerAccount,
-                source: {
-                    isLocalAccount: true,
-                    name: "Lomo",
-                },
-                title: "Lomo",
-                timeZone,
+            const lomoCal = calendars.filter(calendar => {
+                return calendar.title === 'Lomo'
             })
+
+            let calendarId = null;
+            const { timeZone, ownerAccount } = calendars[0]
+
+            if (lomoCal.length != 0) {
+                calendarId = lomoCal.id
+            } else {
+                calendarId = Calendar.createCalendarAsync({
+                    accessLevel: "owner",
+                    allowedReminders: [
+                        "default",
+                        "alert",
+                    ],
+                    allowsModifications: true,
+                    color: "#1E7BA8",
+                    isPrimary: false,
+                    isSynced: true,
+                    isVisible: true,
+                    name: `Lomo-${username}`,
+                    ownerAccount,
+                    source: {
+                        isLocalAccount: true,
+                        name: "Lomo",
+                    },
+                    title: "Lomo",
+                    timeZone,
+                })
+            }
             return Promise.all([timeZone, calendarId])
         })
 }
