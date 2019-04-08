96f5f31df2a09a3540fdf12c8c2c677e94751dd2
Shaq <shobbsdaley@gmail.com>
2019-04-08 15:29:43 +0100

    linking cloud storage, db and auth

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -17,13 +17,34 @@
 // );
 
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import firebaseConnect from "../../firebaseConfig";
 
 export default class Gallery extends Component {
+  state = {
+    images: ""
+  };
+  componentDidMount() {
+    const storage = firebaseConnect.storage().ref();
+    storage
+      .child("/images/1554477181787")
+      .getDownloadURL()
+      .then(url => {
+        console.log(url);
+        this.setState({ images: url });
+      });
+  }
+
   render() {
     return (
-      <View>
-        <Text>Events</Text>
+      <View style={{ flex: 1 }}>
+        <Image
+          style={{ width: 300, height: 500 }}
+          source={{
+            uri:
+              "https://firebasestorage.googleapis.com/v0/b/lomo-snaps.appspot.com/o/images%2F1554477181787?alt=media&token=e008e275-ca56-461e-a7dd-bf93aa63dd71"
+          }}
+        />
       </View>
     );
   }
