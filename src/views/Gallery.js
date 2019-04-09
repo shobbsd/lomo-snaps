5109df089e2a744e4d9c977af1e25505ac18faba
philval <philip.valentino@mac.com>
2019-04-09 16:34:43 +0100

    WIP images URLs from Database collection = album, loading images from Storage

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,27 +1,8 @@
-// import React from "react";
-// import { View, Image, ScrollView } from "react-native";
-
-// import styles from "../styles/cameraStyle";
-
-// export default ({ captures = [] }) => (
-//   <ScrollView
-//     horizontal={true}
-//     style={[styles.bottomToolbar, styles.galleryContainer]}
-//   >
-//     {captures.map(({ uri }) => (
-//       <View style={styles.galleryImageContainer} key={uri}>
-//         <Image source={{ uri }} style={styles.galleryImage} />
-//       </View>
-//     ))}
-//   </ScrollView>
-// );
-
-// https://github.com/firebase/quickstart-js/blob/master/firestore/scripts/FriendlyEats.Data.js
-
 import React, { Component } from "react";
 import { Image } from "react-native";
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
-import firebaseConnect from '../../firebaseConfig'
+// import axios from 'axios';
+import firebaseConnect from '../../firebaseConfig';
 
 // STORAGE
 const storage = firebaseConnect.storage();
@@ -31,24 +12,35 @@ const singleImageRef = storageRef.child('images/1554725383867');
 // DATABASE
 const db = firebaseConnect.firestore()
 
-db.collection("events").get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-    const docData = doc.data()
-    console.log(docData.eventName);
-    // console.log(`${doc.id} => ${(doc.data())}`);
-  });
-});
-
-// const imagesArrayRef = storageRef.child('images');
-// console.log(imagesArrayRef, '<< imagesArrayRef')
-
+// CLASS
 export default class Gallery extends Component {
 
   state = {
-    imageURL: 'https://cdn.dribbble.com/users/421466/screenshots/2379575/replace-2r-400px.gif',   // default loading gif
+    //imageURL: 'https://cdn.dribbble.com/users/421466/screenshots/2379575/replace-2r-400px.gif',   // default loading gif
     imageTimeCreated: new Date(),
+    imageStorageURLs: [],
   }
 
+  // get array of images from album
+  // album is a collection of images and cannot identify which user took the photo
+  getImageStorageURLs = () => {
+    const urls = []
+    db.collection("album").get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const docData = doc.data()
+          urls.push(docData.photoURL)
+        });
+        return urls
+      })
+      .then((urls) => {
+        // console.log(urls, '<< urls')
+        this.setState({ imageStorageURLs: urls })
+        console.log(this.state, '<< this.state')
+      })
+  }
+
+  // TODO remove, single hard-coded image
   getImageURL = () => {
     singleImageRef.getDownloadURL()
       .then(url => { // `url` is the download URL for 'images/stars.jpg'
@@ -58,6 +50,7 @@ export default class Gallery extends Component {
       });
   }
 
+  // TODO remove, single hard-coded image
   // storage meta-data
   getImageTimeCreated = () => {
     singleImageRef.getMetadata()
@@ -71,23 +64,35 @@ export default class Gallery extends Component {
   }
 
   componentDidMount() {
-    this.getImageURL()
-    this.getImageTimeCreated()
+    // this.getImageURL()
+    // this.getImageTimeCreated()
+    this.getImageStorageURLs()
   }
 
   render() {
 
-    const { imageTimeCreated } = this.state
+    const { imageTimeCreated } = this.state;
     // var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };  TODO
     const uiDate = imageTimeCreated.toLocaleDateString('en-GB')
     const uiTime = imageTimeCreated.toLocaleTimeString('en-GB')
 
-    return (
-      <Container>
-        <Header />
-        <Content>
-          <Card>
-            <CardItem>
+    const { imageStorageURLs } = this.state;
+
+    // =map
+    const galleryCards = imageStorageURLs.map(imageStorageURL => {
+
+      const storageRef = firebaseConnect.storage();
+
+      storageRef
+        .refFromURL(`gs://lomo-snaps.appspot.com/images/${imageStorageURL}`)
+        .getDownloadURL()
+        .then(imageURLplusToken => {
+          return imageURLplusToken
+        });
+
+
+      return <Card key={imageStorageURL}>
+        {/* <CardItem>
               <Left>
                 <Thumbnail source={{ uri: 'https://pbs.twimg.com/profile_images/578519382650974209/idoLfRxY_400x400.jpeg' }} />
                 <Body>
@@ -95,26 +100,34 @@ export default class Gallery extends Component {
                   <Text>{uiDate} @ {uiTime}</Text>
                 </Body>
               </Left>
-            </CardItem>
-            <CardItem cardBody>
-              {/* <Image source={{ uri: 'gs://lomo-snaps.appspot.com/images/1554725383867' }} style={{ height: 200, width: null, flex: 1 }} /> */}
-              <Image source={{ uri: this.state.imageURL }} style={{ height: 400, width: null, flex: 1 }} />
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent>
-                  <Icon active name="thumbs-up" />
-                  <Text>12 Likes</Text>
-                </Button>
-              </Left>
-              <Body>
-                <Button transparent>
-                  <Icon active name="chatbubbles" />
-                  <Text>4 Comments</Text>
-                </Button>
-              </Body>
-            </CardItem>
-          </Card>
+            </CardItem> */}
+        <CardItem cardBody>
+          {/* <Image source={{ uri: 'https://lomo-snaps.appspot.com/images/1554725383867' }} style={{ height: 200, width: null, flex: 1 }} /> */}
+          <Image source={{ uri: `https://firebasestorage.googleapis.com/v0/b/lomo-snaps.appspot.com/o/images/${imageStorageURL}?alt=media&token=864a82af-de5d-4d8e-b7a0-c774d3559104` }} style={{ height: 200, width: null, flex: 1 }} />
+          {/* <Image source={{ uri: `gs://lomo-snaps.appspot.com/images/${imageStorageURL}` }} style={{ height: 400, width: null, flex: 1 }} /> */}
+        </CardItem>
+        <CardItem>
+          <Left>
+            <Button transparent>
+              <Icon active name="thumbs-up" />
+              <Text>12 Likes</Text>
+            </Button>
+          </Left>
+          <Body>
+            <Button transparent>
+              <Icon active name="chatbubbles" />
+              <Text>4 Comments</Text>
+            </Button>
+          </Body>
+        </CardItem>
+      </Card>
+    })
+
+    return (
+      <Container>
+        <Header />
+        <Content>
+          {galleryCards}
         </Content>
       </Container>
     );
@@ -123,3 +136,4 @@ export default class Gallery extends Component {
 }
 
 // TODO better loading gif
+// REF https://github.com/firebase/quickstart-js/blob/master/firestore/scripts/FriendlyEats.Data.js
