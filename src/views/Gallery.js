99b9cea634bad027ce9044b20b157f35302e6e63
philval <philip.valentino@mac.com>
2019-04-09 18:37:04 +0100

    WIP Gallery.js

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Image } from "react-native";
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
-// import axios from 'axios';
 import firebaseConnect from '../../firebaseConfig';
 
 // STORAGE
@@ -12,32 +11,54 @@ const singleImageRef = storageRef.child('images/1554725383867');
 // DATABASE
 const db = firebaseConnect.firestore()
 
+function getImageStorageURL(imageDatabaseURLArr) {
+  // const storageRef = firebaseConnect.storage(); // DUPLICATE
+  const finalArr = imageDatabaseURLArr.map(url => {
+    return storage
+      .refFromURL(`gs://lomo-snaps.appspot.com/images/${url}`)
+      .getDownloadURL().then(link => link)
+  })
+  return finalArr
+  // const url = await storage
+  //   .refFromURL(`gs://lomo-snaps.appspot.com/images/${imageDatabaseURL}`)
+  //   .getDownloadURL()
+  //   .then(imageURLplusToken => {
+  //     console.log(imageURLplusToken, '<< imageURLplusToken')
+  //     return imageURLplusToken
+  //   })
+  // console.log(url)
+}
+
 // CLASS
 export default class Gallery extends Component {
 
   state = {
     //imageURL: 'https://cdn.dribbble.com/users/421466/screenshots/2379575/replace-2r-400px.gif',   // default loading gif
     imageTimeCreated: new Date(),
+    imageDatabaseURLs: [],
     imageStorageURLs: [],
   }
 
-  // get array of images from album
+  // get array of images from album from database
   // album is a collection of images and cannot identify which user took the photo
-  getImageStorageURLs = () => {
+  getImageDatabaseURLs = () => {
     const urls = []
     db.collection("album").get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          const docData = doc.data()
-          urls.push(docData.photoURL)
-        });
+
+          const photoURL = doc.data().photoURL
+          urls.push(photoURL)
+        }) // forEach
         return urls
       })
+      // .then((urls) => {
+      //   return this.setState({ imageDatabaseURLs: urls })
+      // })
       .then((urls) => {
-        // console.log(urls, '<< urls')
-        this.setState({ imageStorageURLs: urls })
-        console.log(this.state, '<< this.state')
-      })
+
+        return getImageStorageURL(urls)
+      }).then(urlArr => console.log(urlArr))
   }
 
   // TODO remove, single hard-coded image
@@ -66,32 +87,24 @@ export default class Gallery extends Component {
   componentDidMount() {
     // this.getImageURL()
     // this.getImageTimeCreated()
-    this.getImageStorageURLs()
+    this.getImageDatabaseURLs()
   }
 
   render() {
 
-    const { imageTimeCreated } = this.state;
-    // var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };  TODO
-    const uiDate = imageTimeCreated.toLocaleDateString('en-GB')
-    const uiTime = imageTimeCreated.toLocaleTimeString('en-GB')
+    // const { imageTimeCreated } = this.state;
+    // const uiDate = imageTimeCreated.toLocaleDateString('en-GB')
+    // const uiTime = imageTimeCreated.toLocaleTimeString('en-GB')
 
     const { imageStorageURLs } = this.state;
 
     // =map
     const galleryCards = imageStorageURLs.map(imageStorageURL => {
 
-      const storageRef = firebaseConnect.storage();
+      // const img = getImageStorageURL(imageDatabaseURL)
+      // console.log(img, '<< img')
 
-      storageRef
-        .refFromURL(`gs://lomo-snaps.appspot.com/images/${imageStorageURL}`)
-        .getDownloadURL()
-        .then(imageURLplusToken => {
-          return imageURLplusToken
-        });
-
-
-      return <Card key={imageStorageURL}>
+      return <Card key={Date.now()}>
         {/* <CardItem>
               <Left>
                 <Thumbnail source={{ uri: 'https://pbs.twimg.com/profile_images/578519382650974209/idoLfRxY_400x400.jpeg' }} />
@@ -103,8 +116,8 @@ export default class Gallery extends Component {
             </CardItem> */}
         <CardItem cardBody>
           {/* <Image source={{ uri: 'https://lomo-snaps.appspot.com/images/1554725383867' }} style={{ height: 200, width: null, flex: 1 }} /> */}
-          <Image source={{ uri: `https://firebasestorage.googleapis.com/v0/b/lomo-snaps.appspot.com/o/images/${imageStorageURL}?alt=media&token=864a82af-de5d-4d8e-b7a0-c774d3559104` }} style={{ height: 200, width: null, flex: 1 }} />
-          {/* <Image source={{ uri: `gs://lomo-snaps.appspot.com/images/${imageStorageURL}` }} style={{ height: 400, width: null, flex: 1 }} /> */}
+          {/* <Image source={{ uri: `https://firebasestorage.googleapis.com/v0/b/lomo-snaps.appspot.com/o/images/${img}?alt=media&token=864a82af-de5d-4d8e-b7a0-c774d3559104` }} style={{ height: 200, width: null, flex: 1 }} /> */}
+          <Image source={{ uri: imageStorageURL }} style={{ height: 400, width: null, flex: 1 }} />
         </CardItem>
         <CardItem>
           <Left>
@@ -121,7 +134,7 @@ export default class Gallery extends Component {
           </Body>
         </CardItem>
       </Card>
-    })
+    }) // end Map
 
     return (
       <Container>
