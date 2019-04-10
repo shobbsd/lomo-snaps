3192b436d70b2bfae099a56407ab6bf569360947
Shaq <shobbsdaley@gmail.com>
2019-04-10 19:54:05 +0100

    added live listener for events

--- src/views/EventsList.js
+++ src/views/EventsList.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import {
-    View,
-    Text,
-    ScrollView,
-    StyleSheet,
-    Header,
-    TouchableOpacity,
-    Image
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Header,
+  TouchableOpacity,
+  Image
 } from "react-native";
 import EventCard from "../components/EventCard";
 import { createStackNavigator, createAppContainer } from "react-navigation";
@@ -15,86 +15,101 @@ import "@firebase/firestore";
 import firebaseConnect from "../../firebaseConfig";
 
 export default class EventsList extends Component {
-    state = {
-        events: [],
-        loadedUser: false,
-        user: {}
-    };
+  state = {
+    events: [],
+    loadedUser: false,
+    user: {}
+  };
 
-    componentDidMount() {
-        const { user, events } = this.props.navigation.state.params;
-        this.setState({ user, events });
-    }
+  componentDidMount() {
+    const { user, events } = this.props.navigation.state.params;
+    this.setState({ user });
+    const db = firebaseConnect.firestore();
+    db.collection("events")
+      .where("attendeesUids", "array-contains", user.uid)
+      .onSnapshot(snapshot => {
+        const updateEvents = [];
+        snapshot.docChanges().forEach(change => {
+          updateEvents.push(change.doc.data());
+        });
+        this.setState(state => {
+          console.log(state);
+          if (state.events !== updateEvents)
+            return { events: [...updateEvents, ...state.events] };
+          else return { events: updateEvents };
+        });
+      });
+  }
 
-    componentWillUnmount() {
-        this.props.navigation.state.params.stopLoading();
-    }
+  componentWillUnmount() {
+    this.props.navigation.state.params.stopLoading();
+  }
 
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <Text style={{ fontSize: 30, textAlign: "center", color: "black" }}>
-                    Events
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text style={{ fontSize: 30, textAlign: "center", color: "black" }}>
+          Events
         </Text>
-                <ScrollView>
-                    {this.state.events.map(event => {
-                        return (
-                            <EventCard
-                                handleClick={() => {
-                                    this.handleClick(event);
-                                }}
-                                key={event.eventName}
-                                name={event.eventName}
-                            />
-                        );
-                    })}
-                </ScrollView>
-                <TouchableOpacity
-                    onPress={() => {
-                        this.props.navigation.navigate("NewEvent", {
-                            user: this.state.user
-                        });
-                    }}
-                    style={styles.TouchableOpacityStyle}
-                >
-                    <Image
-                        source={{
-                            uri: "https://img.icons8.com/cotton/2x/plus--v1.png"
-                        }}
-                        style={styles.FloatingButtonStyle}
-                    />
-                </TouchableOpacity>
-            </View>
-        );
-    }
+        <ScrollView>
+          {this.state.events.map(event => {
+            return (
+              <EventCard
+                handleClick={() => {
+                  this.handleClick(event);
+                }}
+                key={event.eventName}
+                name={event.eventName}
+              />
+            );
+          })}
+        </ScrollView>
+        <TouchableOpacity
+          onPress={() => {
+            this.props.navigation.navigate("NewEvent", {
+              user: this.state.user
+            });
+          }}
+          style={styles.TouchableOpacityStyle}
+        >
+          <Image
+            source={{
+              uri: "https://img.icons8.com/cotton/2x/plus--v1.png"
+            }}
+            style={styles.FloatingButtonStyle}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
-    handleClick = event => {
-        this.props.navigation.navigate("Menu", { event });
-    };
+  handleClick = event => {
+    this.props.navigation.navigate("Menu", { event });
+  };
 }
 
 const styles = StyleSheet.create({
-    MainContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#F5F5F5"
-    },
+  MainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5F5F5"
+  },
 
-    TouchableOpacityStyle: {
-        position: "absolute",
-        width: 60,
-        height: 60,
-        alignItems: "center",
-        justifyContent: "center",
-        right: 30,
-        bottom: 30
-    },
+  TouchableOpacityStyle: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    right: 30,
+    bottom: 30
+  },
 
-    FloatingButtonStyle: {
-        resizeMode: "contain",
-        width: 60,
-        height: 60
-        //backgroundColor:'black'
-    }
+  FloatingButtonStyle: {
+    resizeMode: "contain",
+    width: 60,
+    height: 60
+    //backgroundColor:'black'
+  }
 });
